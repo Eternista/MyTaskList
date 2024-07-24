30fe6170407bfeb10270bd813d88bcb058eba560
Eternista <rafal.ziolkowski1234@gmail.com>
2024-07-24 22:53:08 +0200

    UPDATE FOR 24.07

---
+++ src/components/extras/Linesparator.tsx
@@ -0,0 +1,9 @@
+interface SeparatorProps {
+    color: string;
+}
+
+const LineSeparator = ({color} : SeparatorProps) => {
+    return <div className={`lineseparator lineseparator-${color}`}></div>
+}
+
+export default LineSeparator;
